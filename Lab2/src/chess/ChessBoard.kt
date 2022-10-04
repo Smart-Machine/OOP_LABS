5b7b053b3f18ade955b3d47a070ef412f76096b2
Smart-Machine <radu.calin500@gmail.com>
2022-10-05 01:21:38 +0300

    simulation added

--- Lab2/src/chess/ChessBoard.kt
+++ Lab2/src/chess/ChessBoard.kt
@@ -52,11 +52,13 @@ class ChessBoard {
             CharArray(8) {'.'},
         ) 
 
+    private var whosTurn = "white"
+
 
     constructor() {
-        updateBoard()
-        drawBoard()
-        simulateGame()
+        // updateBoard()
+        // drawBoard()
+        // simulateGame()
     }
 
     fun updateBoard() {
@@ -73,61 +75,61 @@ class ChessBoard {
         ) 
 
 
-        // // Put left black Rook. 
-        // board[leftBlackRook.get_row()][leftBlackRook.get_column()] = leftBlackRook.symbol
-        // // Put right black Rook.
-        // board[rightBlackRook.get_row()][rightBlackRook.get_column()] = rightBlackRook.symbol
-        // // Put left white Rook.
-        // board[leftWhiteRook.get_row()][leftWhiteRook.get_column()] = leftWhiteRook.symbol
+        // Put left black Rook. 
+        board[leftBlackRook.get_row()][leftBlackRook.get_column()] = leftBlackRook.symbol
+        // Put right black Rook.
+        board[rightBlackRook.get_row()][rightBlackRook.get_column()] = rightBlackRook.symbol
+        // Put left white Rook.
+        board[leftWhiteRook.get_row()][leftWhiteRook.get_column()] = leftWhiteRook.symbol
         // Put right white Rook.
-        // board[rightWhiteRook.get_row()][rightWhiteRook.get_column()] = rightWhiteRook.symbol
+        board[rightWhiteRook.get_row()][rightWhiteRook.get_column()] = rightWhiteRook.symbol
 
-        // // Put left black Knight.
-        // board[leftBlackKnight.get_row()][leftBlackKnight.get_column()] = leftBlackKnight.symbol
-        // // Put right black Knight.
-        // board[rightBlackKnight.get_row()][rightBlackKnight.get_column()] = rightBlackKnight.symbol
-        // // Put left white Knight.
+        // Put left black Knight.
+        board[leftBlackKnight.get_row()][leftBlackKnight.get_column()] = leftBlackKnight.symbol
+        // Put right black Knight.
+        board[rightBlackKnight.get_row()][rightBlackKnight.get_column()] = rightBlackKnight.symbol
+        // Put left white Knight.
         board[leftWhiteKnight.get_row()][leftWhiteKnight.get_column()] = leftWhiteKnight.symbol
-        // // Put right white Knight.
-        // board[rightWhiteKnight.get_row()][rightWhiteKnight.get_column()] = rightWhiteKnight.symbol
+        // Put right white Knight.
+        board[rightWhiteKnight.get_row()][rightWhiteKnight.get_column()] = rightWhiteKnight.symbol
 
-        // // Put left black Bishop.
-        // board[leftBlackBishop.get_row()][leftBlackBishop.get_column()] = leftBlackBishop.symbol
-        // // Put right black Bishop.
-        // board[rightBlackBishop.get_row()][rightBlackBishop.get_column()] = rightBlackBishop.symbol
-        // // Put left white Bishop.
-        // board[leftWhiteBishop.get_row()][leftWhiteBishop.get_column()] = leftWhiteBishop.symbol
-        // // Put right white Bishop.
-        // board[rightWhiteBishop.get_row()][rightWhiteBishop.get_column()] = rightWhiteBishop.symbol
+        // Put left black Bishop.
+        board[leftBlackBishop.get_row()][leftBlackBishop.get_column()] = leftBlackBishop.symbol
+        // Put right black Bishop.
+        board[rightBlackBishop.get_row()][rightBlackBishop.get_column()] = rightBlackBishop.symbol
+        // Put left white Bishop.
+        board[leftWhiteBishop.get_row()][leftWhiteBishop.get_column()] = leftWhiteBishop.symbol
+        // Put right white Bishop.
+        board[rightWhiteBishop.get_row()][rightWhiteBishop.get_column()] = rightWhiteBishop.symbol
 
-        // // Put black Queen.
-        // board[blackQueen.get_row()][blackQueen.get_column()] = blackQueen.symbol
-        // // Put white Queen.
-        // board[whiteQueen.get_row()][whiteQueen.get_column()] = whiteQueen.symbol
+        // Put black Queen.
+        board[blackQueen.get_row()][blackQueen.get_column()] = blackQueen.symbol
+        // Put white Queen.
+        board[whiteQueen.get_row()][whiteQueen.get_column()] = whiteQueen.symbol
 
-        // // Put black King.
-        // board[blackKing.get_row()][blackKing.get_column()] = blackKing.symbol
-        // // Put white King.
-        // board[whiteKing.get_row()][whiteKing.get_column()] = whiteKing.symbol
+        // Put black King.
+        board[blackKing.get_row()][blackKing.get_column()] = blackKing.symbol
+        // Put white King.
+        board[whiteKing.get_row()][whiteKing.get_column()] = whiteKing.symbol
 
-        // // Put black Pawns.
-        // board[blackPawn1.get_row()][blackPawn1.get_column()] = blackPawn1.symbol
-        // board[blackPawn2.get_row()][blackPawn2.get_column()] = blackPawn2.symbol
-        // board[blackPawn3.get_row()][blackPawn3.get_column()] = blackPawn3.symbol
-        // board[blackPawn4.get_row()][blackPawn4.get_column()] = blackPawn4.symbol
-        // board[blackPawn5.get_row()][blackPawn5.get_column()] = blackPawn5.symbol
-        // board[blackPawn6.get_row()][blackPawn6.get_column()] = blackPawn6.symbol
-        // board[blackPawn7.get_row()][blackPawn7.get_column()] = blackPawn7.symbol
-        // board[blackPawn8.get_row()][blackPawn8.get_column()] = blackPawn8.symbol
-        // // Put white Pawns.
-        // board[whitePawn1.get_row()][whitePawn1.get_column()] = whitePawn1.symbol
-        // board[whitePawn2.get_row()][whitePawn2.get_column()] = whitePawn2.symbol
-        // board[whitePawn3.get_row()][whitePawn3.get_column()] = whitePawn3.symbol
-        // board[whitePawn4.get_row()][whitePawn4.get_column()] = whitePawn4.symbol
-        // board[whitePawn5.get_row()][whitePawn5.get_column()] = whitePawn5.symbol
-        // board[whitePawn6.get_row()][whitePawn6.get_column()] = whitePawn6.symbol
-        // board[whitePawn7.get_row()][whitePawn7.get_column()] = whitePawn7.symbol
-        // board[whitePawn8.get_row()][whitePawn8.get_column()] = whitePawn8.symbol
+        // Put black Pawns.
+        board[blackPawn1.get_row()][blackPawn1.get_column()] = blackPawn1.symbol
+        board[blackPawn2.get_row()][blackPawn2.get_column()] = blackPawn2.symbol
+        board[blackPawn3.get_row()][blackPawn3.get_column()] = blackPawn3.symbol
+        board[blackPawn4.get_row()][blackPawn4.get_column()] = blackPawn4.symbol
+        board[blackPawn5.get_row()][blackPawn5.get_column()] = blackPawn5.symbol
+        board[blackPawn6.get_row()][blackPawn6.get_column()] = blackPawn6.symbol
+        board[blackPawn7.get_row()][blackPawn7.get_column()] = blackPawn7.symbol
+        board[blackPawn8.get_row()][blackPawn8.get_column()] = blackPawn8.symbol
+        // Put white Pawns.
+        board[whitePawn1.get_row()][whitePawn1.get_column()] = whitePawn1.symbol
+        board[whitePawn2.get_row()][whitePawn2.get_column()] = whitePawn2.symbol
+        board[whitePawn3.get_row()][whitePawn3.get_column()] = whitePawn3.symbol
+        board[whitePawn4.get_row()][whitePawn4.get_column()] = whitePawn4.symbol
+        board[whitePawn5.get_row()][whitePawn5.get_column()] = whitePawn5.symbol
+        board[whitePawn6.get_row()][whitePawn6.get_column()] = whitePawn6.symbol
+        board[whitePawn7.get_row()][whitePawn7.get_column()] = whitePawn7.symbol
+        board[whitePawn8.get_row()][whitePawn8.get_column()] = whitePawn8.symbol
     }
 
     fun drawBoard() {
@@ -143,9 +145,43 @@ class ChessBoard {
     }
 
     fun simulateGame() {
-        leftWhiteKnight.move("a3", board)
+        when (whosTurn) {
+            "white" -> {
+                println("White To Move")
+                var whitePawns = arrayOf(
+                    whitePawn1,
+                    whitePawn2,
+                    whitePawn3,
+                    whitePawn4, 
+                    whitePawn5,
+                    whitePawn6,
+                    whitePawn7,
+                    whitePawn8
+                )
+                var piece = whitePawns.random()
+                var next_move = piece.get_available_moves(board)
+                piece.move(next_move.random(), board)
+                whosTurn = "black"
+            }
+            "black" -> {
+                println("Black To Move")
+                var blackPawns = arrayOf(
+                    blackPawn1,
+                    blackPawn2,
+                    blackPawn3,
+                    blackPawn4,
+                    blackPawn5,
+                    blackPawn6,
+                    blackPawn7,
+                    blackPawn8
+                )
+                var piece = blackPawns.random()
+                var next_move = piece.get_available_moves(board)
+                piece.move(next_move.random(), board)
+                whosTurn = "white"
+            }
+        }
         updateBoard()
         drawBoard()
-
     }
 }
--- Lab2/src/main.kt
+++ Lab2/src/main.kt
@@ -2,4 +2,8 @@ import chess.*
 
 fun main() {
     val chessBoard = ChessBoard()
+    chessBoard.updateBoard()
+    chessBoard.drawBoard()
+    for (turns in 1..7)
+        chessBoard.simulateGame()
 }
